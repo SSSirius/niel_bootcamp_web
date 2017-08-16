8d987106b0f5a89ae20e917f4287cca942832fa2
SSSirius <seed0480@gmail.com>
2017-08-16 00:36:18 -0400

    d2

--- final/js/main.js
+++ final/js/main.js
@@ -2,69 +2,69 @@
 ///////////////////////////////////////////////////////////////////////////
 /////////////////////// Gradients per Planet or Total /////////////////////
 ///////////////////////////////////////////////////////////////////////////
-function createGradients() {
+// function createGradients() {
 
-	//Just for fun a gradient that runs over all planets in a rainbow patterns
-	var gradientLinear = svg
-		.append("linearGradient")
-		.attr("id", "gradientLinear")
-		.attr("gradientUnits", "userSpaceOnUse") 
-		.attr("y1", "0")
-		.attr("y2", "0")
-		.attr("x1", "0")
-		.attr("x2", "30%")
-		.attr("spreadMethod", "reflect")
-		.selectAll("stop") 
-		.data([                             
-				{offset: "0%", color: "#6363FF"}, 
-				{offset: "6.16%", color: "#6373FF"}, 
-				{offset: "12.4%", color: "#63A3FF"}, 
-				{offset: "18.7%", color: "#63E3FF"}, 
-				{offset: "24.9%", color: "#63FFFB"}, 
-				{offset: "31.2%", color: "#63FFCB"}, 
-				{offset: "37.5%", color: "#63FF9B"}, 
-				{offset: "43.7%", color: "#63FF6B"}, 
-				{offset: "50%", color: "#7BFF63"}, 
-				{offset: "56.3%", color: "#BBFF63"}, 
-				{offset: "62.5%", color: "#DBFF63"}, 
-				{offset: "68.8%", color: "#FBFF63"}, 
-				{offset: "75%", color: "#FFD363"}, 
-				{offset: "81.3%", color: "#FFB363"}, 			
-				{offset: "87.6%", color: "#FF8363"},   
-				{offset: "93.8%", color: "#FF7363"}, 			
-				{offset: "100%", color: "#FF6364"}    
-			])                  
-		.enter().append("stop") 
-		.attr("offset", function(d) { return d.offset; })   
-		.attr("stop-color", function(d) { return d.color; });
+// 	//Just for fun a gradient that runs over all planets in a rainbow patterns
+// 	var gradientLinear = svg
+// 		.append("linearGradient")
+// 		.attr("id", "gradientLinear")
+// 		.attr("gradientUnits", "userSpaceOnUse") 
+// 		.attr("y1", "0")
+// 		.attr("y2", "0")
+// 		.attr("x1", "0")
+// 		.attr("x2", "30%")
+// 		.attr("spreadMethod", "reflect")
+// 		.selectAll("stop") 
+// 		.data([                             
+// 				{offset: "0%", color: "#6363FF"}, 
+// 				{offset: "6.16%", color: "#6373FF"}, 
+// 				{offset: "12.4%", color: "#63A3FF"}, 
+// 				{offset: "18.7%", color: "#63E3FF"}, 
+// 				{offset: "24.9%", color: "#63FFFB"}, 
+// 				{offset: "31.2%", color: "#63FFCB"}, 
+// 				{offset: "37.5%", color: "#63FF9B"}, 
+// 				{offset: "43.7%", color: "#63FF6B"}, 
+// 				{offset: "50%", color: "#7BFF63"}, 
+// 				{offset: "56.3%", color: "#BBFF63"}, 
+// 				{offset: "62.5%", color: "#DBFF63"}, 
+// 				{offset: "68.8%", color: "#FBFF63"}, 
+// 				{offset: "75%", color: "#FFD363"}, 
+// 				{offset: "81.3%", color: "#FFB363"}, 			
+// 				{offset: "87.6%", color: "#FF8363"},   
+// 				{offset: "93.8%", color: "#FF7363"}, 			
+// 				{offset: "100%", color: "#FF6364"}    
+// 			])                  
+// 		.enter().append("stop") 
+// 		.attr("offset", function(d) { return d.offset; })   
+// 		.attr("stop-color", function(d) { return d.color; });
 
-	//Radial gradient with the center at one end of the circle, as if illuminated from the side
-	//A gradient is created for each planet and colored to the temperature of its star
-	var gradientContainer = container.append("g").attr("class","gradientContainer");
+// 	//Radial gradient with the center at one end of the circle, as if illuminated from the side
+// 	//A gradient is created for each planet and colored to the temperature of its star
+// 	var gradientContainer = container.append("g").attr("class","gradientContainer");
 
-	var gradientRadial = gradientContainer
-		.selectAll("radialGradient").data(planets).enter()
-		.append("radialGradient")
-		.attr("cx", "50%")
-		.attr("cy", "50%")
-		.attr("r", "50%")
-		.attr("fx", "0%")
-		.attr("gradientUnits", "objectBoundingBox")
-		.attr('id', function(d){return "gradientRadial-"+d.ID})
+// 	var gradientRadial = gradientContainer
+// 		.selectAll("radialGradient").data(planets).enter()
+// 		.append("radialGradient")
+// 		.attr("cx", "50%")
+// 		.attr("cy", "50%")
+// 		.attr("r", "50%")
+// 		.attr("fx", "0%")
+// 		.attr("gradientUnits", "objectBoundingBox")
+// 		.attr('id', function(d){return "gradientRadial-"+d.ID})
 
-	gradientRadial.append("stop")
-		.attr("offset", "0%")
-		.attr("stop-color", function(d) {return d3.rgb(colorScale(d.temp)).brighter(1);});
+// 	gradientRadial.append("stop")
+// 		.attr("offset", "0%")
+// 		.attr("stop-color", function(d) {return d3.rgb(colorScale(d.temp)).brighter(1);});
 
-	gradientRadial.append("stop")
-		.attr("offset", "40%")
-		.attr("stop-color", function(d) {return colorScale(d.temp);});
+// 	gradientRadial.append("stop")
+// 		.attr("offset", "40%")
+// 		.attr("stop-color", function(d) {return colorScale(d.temp);});
 		 
-	gradientRadial.append("stop")
-		.attr("offset",  "100%")
-		.attr("stop-color", function(d) {return d3.rgb(colorScale(d.temp)).darker(1.75);});
+// 	gradientRadial.append("stop")
+// 		.attr("offset",  "100%")
+// 		.attr("stop-color", function(d) {return d3.rgb(colorScale(d.temp)).darker(1.75);});
 	
-};
+// };
 // //Width and Height of the SVG
 // var 
 // 	w = window,
@@ -119,19 +119,20 @@ function createGradients() {
 // 	.attr("text-anchor", "middle");	
 
 //Width and Height of the SVG
+var isstart = true;
 var 
 	w = window,
 	d = document,
 	e = d.documentElement,
 	g = d.getElementsByTagName('body')[0],
 	x = (w.innerWidth || e.clientWidth || g.clientWidth) - 10,
-	y = (w.innerHeight|| e.clientHeight|| g.clientHeight);
+	y = (w.innerHeight|| e.clientHeight|| g.clientHeight)-10;
 
 
 window.onresize = updateWindow;	
 function updateWindow(){
 	x = (w.innerWidth || e.clientWidth || g.clientWidth) - 10;
-	y = (w.innerHeight|| e.clientHeight|| g.clientHeight);
+	y = (w.innerHeight|| e.clientHeight|| g.clientHeight)-10;
 
 	svg.attr("width", x).attr("height", y);
 	// d3.selectAll(".container").attr("transform", "translate3d(2,2,0)");
@@ -177,54 +178,48 @@ var container = svg.append("g").attr("class","container")
 //Create star in the Middle - scaled to the orbits
 //Radius of our Sun in these coordinates (taking into account size of circle inside image)
 var ImageWidth = radiusSun/au * 3000 * (2.7/1.5);
-container.
-append("svg:image")
-	.attr("x", -ImageWidth)
-	.attr("y", -ImageWidth)
-	.attr("class", "sun")
-	.attr("xlink:href", "img/sun.png")
-	.attr("width", ImageWidth*2)
-	.attr("height", ImageWidth*2)
-	.attr("text-anchor", "middle");	
+
 
 //d3.json("exoplanets.json", function(error, planets) {
-var orbits =[
-			
-			
-			{cx:0,cy:0,major:475,minor:475,Radius:4},
-			{cx:0,cy:0,major:260,minor:260,Radius:4},
-			{cx:0,cy:0,major:75,minor:75,Radius:4},
-			{cx:0,cy:0,major:50,minor:50,Radius:4},
-			{cx:0,cy:0,major:35,minor:35,Radius:4},
-			{cx:0,cy:0,major:20,minor:20,Radius:4}
-
-
-			]
 // var orbits =[
 			
-// 			{cx:0,cy:0,major:475,minor:200,Radius:4},
-// 			{cx:0,cy:0,major:260,minor:109.5,Radius:4},
-// 			{cx:0,cy:0,major:75,minor:31.6,Radius:4},
-// 			{cx:0,cy:0,major:50,minor:21,Radius:4},
-// 			{cx:0,cy:0,major:35,minor:14.7,Radius:4},
-// 			{cx:0,cy:0,major:20,minor:8.42,Radius:4}
+			
+// 			{name:"saturnO",cx:0,cy:0,major:475,minor:475,Radius:4},
+// 			{name:"jupiterO",cx:0,cy:0,major:260,minor:260,Radius:4},
+// 			{name:"marsO",cx:0,cy:0,major:75,minor:75,Radius:4},
+// 			{name:"earthO",cx:0,cy:0,major:50,minor:50,Radius:4},
+// 			{name:"venusO",cx:0,cy:0,major:35,minor:35,Radius:4},
+// 			{name:"mercuryO",cx:0,cy:0,major:20,minor:20,Radius:4}
+
 
 // 			]
+var orbits =[
+			{name:"neptuneO",cx:0,cy:0,major:350,minor:175,Radius:4},
+			{name:"uranusO",cx:0,cy:0,major:292,minor:147.55,Radius:4},
+			{name:"saturnO",cx:0,cy:0,major:132,minor:66,Radius:4},
+			{name:"jupiterO",cx:0,cy:0,major:92,minor:46,Radius:4},
+			{name:"marsO",cx:0,cy:0,major:58,minor:29,Radius:4},
+			{name:"earthO",cx:0,cy:0,major:52,minor:26,Radius:4},
+			{name:"venusO",cx:0,cy:0,major:48,minor:24,Radius:4},
+			{name:"mercuryO",cx:0,cy:0,major:45,minor:22.5,Radius:4}
+
+			]
 
 var planets =[  
-				// {Radius:60,orbit:475,speed:0.05,x:-475,y:-200,href:"img/saturn.png"},
-				// {Radius:28,orbit:260,speed:0.127,x:-260,y:-109.5,href:"img/jupiter.png"},
-				// {Radius:15,orbit:75,speed:0.8,x:-75,y:-31.6,href:"img/mars.png"},
-				// {Radius:15,orbit:50,speed:1.5,x:-50,y:-21,href:"img/earth.png"},
-				// {Radius:15,orbit:35,speed:2.43,x:-35,y:-14.7,href:"img/venus.png"},
-				// {Radius:15,orbit:20,speed:6.2,x:-20,y:-8.42,href:"img/mercury.png"}
-
-				{Radius:60,orbit:475,speed:0.05,x:-475,y:-475,href:"img/saturn.png"},
-				{Radius:28,orbit:260,speed:0.127,x:-260,y:-260,href:"img/jupiter.png"},
-				{Radius:15,orbit:75,speed:0.8,x:-75,y:-75,href:"img/mars.png"},
-				{Radius:15,orbit:50,speed:1.5,x:-50,y:-50,href:"img/earth.png"},
-				{Radius:15,orbit:35,speed:2.43,x:-35,y:-35,href:"img/venus.png"},
-				{Radius:15,orbit:20,speed:6.2,x:-20,y:-20,href:"img/mercury.png"}
+				{name:"mercury",Radius:10,orbit:35,speed:6.2,x:-45,y:-22.5,href:"img/mercury.png"},
+				{name:"venus",Radius:10,orbit:48,speed:2.43,x:-48,y:-24,href:"img/venus.png"},
+				{name:"earth",Radius:10,orbit:52,speed:1.5,x:-52,y:-26,href:"img/earth.png"},
+				{name:"mars",Radius:10,orbit:58,speed:0.8,x:-58,y:-29,href:"img/mars.png"},
+				{name:"jupiter",Radius:50,orbit:92,speed:0.127,x:-92,y:-46,href:"img/jupiter.png"},
+				{name:"saturn",Radius:80,orbit:132,speed:0.05,x:132,y:66,href:"img/saturn.png"},
+				{name:"uranus",Radius:10,orbit:292,speed:0.0177,x:-292,y:-147.55,href:"img/uranus.png"},
+				{name:"neptune",Radius:8,orbit:350,speed:0.009,x:350,y:175,href:"img/neptune.png"}
+				// {name:"saturn",Radius:60,orbit:300,speed:0.05,x:-300,y:-300,href:"img/saturn.png"},
+				// {name:"jupiter",Radius:28,orbit:174.7,speed:0.127,x:-174.7,y:-174.7,href:"img/jupiter.png"},
+				// {name:"mars",Radius:15,orbit:67,speed:0.8,x:-67,y:-67,href:"img/mars.png"},
+				// {name:"earth",Radius:15,orbit:52.4,speed:1.5,x:-52.4,y:-52.4,href:"img/earth.png"},
+				// {name:"venus",Radius:15,orbit:43.7,speed:2.43,x:-43.7,y:-43.7,href:"img/venus.png"},
+				// {name:"mercury",Radius:15,orbit:35,speed:6.2,x:-35,y:-35,href:"img/mercury.png"}
 
 
 				]
@@ -247,8 +242,8 @@ var rScale = d3.scale.linear()
 var formatSI = d3.format(".2f");
 
 // //Create the gradients for the planet fill
-var gradientChoice = "Temp";
-createGradients();
+// var gradientChoice = "Temp";
+// createGradients();
 
 // ///////////////////////////////////////////////////////////////////////////
 // /////////////////////////// Plot and move planets /////////////////////////
@@ -260,47 +255,36 @@ var orbitsContainer = container.append("g").attr("class","orbitsContainer");
 var orbits = orbitsContainer.selectAll("g.orbit")
 				.data(orbits).enter().append("ellipse")
 				.attr("class", "orbit")
+				.attr("id", function(d) {return d.name;})
 				.attr("cx", function(d) {return d.cx;})
 				.attr("cy", function(d) {return d.cy;})
-				.attr("rx", function(d) {return d.major*2.8;})
-				.attr("ry", function(d) {return d.minor*2.2;})
+				.attr("rx", function(d) {return d.major*2.2;})
+				.attr("ry", function(d) {return d.minor*1.5;})
 				.style("fill", "#3E5968")
 				.style("fill-opacity", 0)
 				.style("stroke", "#bbb")
 				.style("stroke-opacity", 0.3)
-				.style("stroke-width", "1px")
+				.style("stroke-width", "0.5px")
 				.on("mouseover", planetOver)
-				.on("click", scaleLarge)
+				.on("click", pause)
 				.on("mouseout", planetOut);
 
 //Drawing the planets			
 var planetContainer = container.append("g").attr("class","planetContainer");
-var ifscale = false;
-planets = planetContainer.selectAll("image")
-				.data(planets).enter().append("image")
-				// .append("g")
-				// .attr("class", "planetWrap")					
-				// .append("circle")
-				.attr("class", "planet")
-				// .attr("r", function(d) {return radiusSizer*d.Radius;})//rScale(d.Radius);})
-				// .attr("cx", function(d) {return d.x;})
-				// .attr("cy", function(d) {return d.y;})
-				// // .style("fill", function(d){return "url(#gradientRadial-" + d.ID + ")";})
-				// .style("fill", "#fff")
 
-				// .style("opacity", 1)
-				// .style("stroke-opacity", 0)
-				// .style("stroke-width", "3px")
-				// .style("stroke", "white")
+				var ifscale = false;
+
+				planets = planetContainer.selectAll("image")
+				.data(planets).enter().append("image")
+				.attr("class", "planet")
 				.attr("x",function(d) {return d.x;})
 				.attr("y", function(d) {return d.y;})
 				// .attr("class", "earth")
 				.attr("xlink:href", function(d) {return d.href;})
 				.attr("width",  function(d) {return d.Radius} )
 				.attr("height", function(d) {return d.Radius} )
-				.attr("text-anchor", "middle");
-				
-				// .on("mouseout", planetOut);
+				.attr("text-anchor", "middle")
+				.on("click", pause)
 				function scaleLarge(d){
 					// orbit.stop();
 					// d3.select(this).style("fill-opacity", 0.3);
@@ -334,6 +318,14 @@ planets = planetContainer.selectAll("image")
 					// Draw1();
 					// d3.select(this).style("background","#fff");
 				}
+				planetContainer.append("g:image")
+	.attr("x", -ImageWidth*1.2)
+	.attr("y", -ImageWidth*1.2)
+	.attr("class", "sun")
+	.attr("xlink:href", "img/sun.png")
+	.attr("width", ImageWidth*2.4)
+	.attr("height", ImageWidth*2.4)
+	.attr("text-anchor", "middle");	
 
 				// .on("mouseover", function(d, i) {
 				// 	stopTooltip = false;					
@@ -564,28 +556,28 @@ Draw1();
 // //////////////////////// Storytelling steps ///////////////////////////////
 // ///////////////////////////////////////////////////////////////////////////	
 
-function Draw0(){
+// function Draw0(){
 
-	stopTooltip = true;	
+// 	stopTooltip = true;	
 	
-	//Make other buttons invisible as to not distract
-	// d3.select("#start").transition().duration(1000).style("opacity", 0);
-	//Remove button
-	setTimeout(function() {
-		d3.select("#start")
-			.style("visibility","hidden");
-		}, 1200);
+// 	//Make other buttons invisible as to not distract
+// 	// d3.select("#start").transition().duration(1000).style("opacity", 0);
+// 	//Remove button
+// 	setTimeout(function() {
+// 		d3.select("#start")
+// 			.style("visibility","hidden");
+// 		}, 1200);
 	
 
-	//Make legend invisible as to not distract
-	d3.select(".legendContainer").transition().duration(1000).style("opacity", 0);
-	d3.select(".introWrapper").transition().duration(1000).style("opacity", 0);
+// 	//Make legend invisible as to not distract
+// 	d3.select(".legendContainer").transition().duration(1000).style("opacity", 0);
+// 	d3.select(".introWrapper").transition().duration(1000).style("opacity", 0);
 	
-	//Remove event listeners during examples
-	removeEvents();
+// 	//Remove event listeners during examples
+// 	removeEvents();
 							
-	//Start
-	startCircle(time = 29);
+// 	//Start
+// 	startCircle(time = 29);
 	
 // 	changeText("Let me introduce you to this chaos of exoplanets that orbit many " + 
 // 			   "different stars in our Milky Way", 
@@ -613,10 +605,18 @@ function Draw0(){
 // 			   "a pixel if I keep them on these orbits. ",
 // 				delayDisappear = 27, delayAppear = 28);
 
-}//function Draw0
+//function Draw0
 var countstep= 0;
 
-
+// console.log(isstart);
+ function pause() {
+		if(isstart)
+			{isstart = false;}
+		else {
+		 	isstart = true;
+		 	Draw1()
+		 } 
+		};
 
 // console.log(countstep);
 //Scaling radii	
@@ -637,7 +637,7 @@ function Draw1() {
 	d3.selectAll(".planet")
 		// .transition().delay(700 * 2).duration(2000)
 		.attr("x", function(d) {
-			var newRadius = -d.x*2.8 * Math.sin(counter*d.speed*Math.PI/180)-d.Radius/2;
+			var newRadius = -d.x*2.2 * Math.sin(counter*d.speed*Math.PI/180)-d.Radius/2;
 			// console.log(countstep);
 			// if  (newRadius > 500) {return 0;}
 			// else {
@@ -645,28 +645,33 @@ function Draw1() {
 			// }
 		})
 		.attr("y", function(d) {
-			var newRadius = -d.y*2.2* Math.cos(counter*d.speed*Math.PI/180)-d.Radius/2;
+			var newRadius = -d.y*1.5* Math.cos(counter*d.speed*Math.PI/180)-d.Radius/2;
 			// console.log(d3.select(".progressWrapper"));
 			// if  (newRadius > 500) {return 0;}
 			// else {
 				return newRadius;
 			// }
 		});
-		counter = counter + 1;
-		t=setTimeout("Draw1()",50);
-}//function Draw1
+		// console.log(isstart);
+		if (isstart){
+			counter = counter + 1;
+		    t=setTimeout("Draw1()",50);}
+	
+       }
+     
+//function Draw1
 
 //Radius of orbit
-function Draw2() {
+// function Draw2() {
 
-	startCircle(time = 26);
+// 	startCircle(time = 26);
 	
-	//Dim all planets again
-	// dim(delayTime = 0);
-	//Make planets bigger again
-	d3.selectAll(".planet")
-		.transition().delay(700 * 1).duration(1500)
-		.attr("x", function(d) {return radiusSizer * d.Radius;});		
+// 	//Dim all planets again
+// 	// dim(delayTime = 0);
+// 	//Make planets bigger again
+// 	d3.selectAll(".planet")
+// 		.transition().delay(700 * 1).duration(1500)
+// 		.attr("x", function(d) {return radiusSizer * d.Radius;});		
 
 	//Highlight the biggest planet
 	// highlight(235, delayTime=4);
@@ -681,11 +686,11 @@ function Draw2() {
 	// changeText("This is extremely close. Even Mercury, the planet closest to our Sun, is stil 0.3 au away, which " +
 	// 		   "would not fit on most regular screen sizes ",
 	// 			delayDisappear = 24, delayAppear = 25);						
-}//Draw2
+// }//Draw2
 
 //Orbital period
-function Draw3(){
-	startCircle(time = 18);
+// function Draw3(){
+// 	startCircle(time = 18);
 	
 	// changeText("The planets you see here are quite different from Earth because of more reasons. " +
 	// 		   "The average time it takes these 288 planets to go around their star is only 17 Earth days! ",
@@ -699,7 +704,7 @@ function Draw3(){
 	// changeText("and Kepler-68 c in almost 10 days",
 	// 			delayDisappear = 16, delayAppear = 17);			
 
-}//Draw3
+// }//Draw3
 		
 
 // //Elliptical orbits - Circles
